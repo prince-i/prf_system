4b7f905442454381e440bb9ea1aa4cc35ce66201
Prince <prince_arce1601@outlook.com>
2021-02-01 16:53:14 +0800

    PRF

--- auto_sync.php
+++ auto_sync.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" href="Img/logo.jpg" type="image/jpg" sizes="16x16">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PRF Auto-Sync</title>
